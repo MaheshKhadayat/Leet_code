caee0528b5d9b15b88425cf0662261f1a06a2976
MaheshKhadayat <khadayatmahesh198@gmail.com>
2026-08-02 17:53:38 +0530

    Time: 0 ms (100%), Space: 12.2 MB (99.6%) - LeetHub

--- 0877-stone-game/0877-stone-game.py
+++ 0877-stone-game/0877-stone-game.py
@@ -4,4 +4,21 @@ class Solution(object):
         :type piles: List[int]
         :rtype: bool
         """
-        return  True
+        alice_score = bob_score = 0
+
+        st = 0
+        end = len(piles)-1
+        i = 0
+        while st < end:
+            stones = max(piles[st],piles[end])
+            if piles[st] >= piles[end]:
+                st+=1
+            else:
+                end-=1
+            if i % 2 == 0:
+                alice_score+=stones
+            else:
+                bob_score+=stones
+            
+        return True if alice_score > bob_score else False
+            
