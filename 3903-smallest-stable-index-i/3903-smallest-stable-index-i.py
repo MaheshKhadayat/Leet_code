8466ddc19ab1a1b7d76008b3623290bd9930f462
Mahesh Khadayat <khadayatmahesh198@gmail.com>
2026-09-08 11:59:50 +0530

    Time: 7 ms (66.86%), Space: 12.5 MB (34.31%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.py
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.py
@@ -5,12 +5,17 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        ans = -1
-        for i in range(len(nums)):
-            maxele = max(nums[:i+1])
+        n = len(nums)
+        maxele = nums[0]
+        for i in range(n):
             minele = min(nums[i:])
-            
-            if maxele - minele <= k:
-                ans = i
-                break
+            maxele = max(maxele,nums[i])
+
+            if maxele-minele <= k:
+                return i
+
+        return -1
+
+        
+
         return ans
